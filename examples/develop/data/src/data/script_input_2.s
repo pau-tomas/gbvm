f685d23e9ad1fef12c361fc0cb0182b2a88bf177
Toxa <untoxa@mail.ru>
2022-08-16 01:23:58 +0300

    make everything (almost, except parts of UI and flasher) autobanked

--- examples/develop/data/src/data/script_input_2.s
+++ examples/develop/data/src/data/script_input_2.s
@@ -31,10 +31,10 @@ _script_input_2::
         VM_OVERLAY_MOVE_TO      0, 0, .OVERLAY_IN_SPEED
         VM_OVERLAY_WAIT         .UI_MODAL, ^/(.UI_WAIT_WINDOW | .UI_WAIT_BTN_A)/
 
-        ; Print 
+        ; Print
         VM_PUSH_CONST           0
         VM_PRINTER_DETECT       .ARG0, 10
-        VM_IF_CONST     .NE     .ARG0, 0, 103$, 0 
+        VM_IF_CONST     .NE     .ARG0, 0, 103$, 0
         VM_PRINT_OVERLAY        .ARG0, 0, 18
 103$:
         VM_POP                  1
@@ -119,10 +119,10 @@ _script_input_2::
         VM_DISPLAY_TEXT
         VM_OVERLAY_WAIT         .UI_MODAL, ^/(.UI_WAIT_WINDOW | .UI_WAIT_TEXT | .UI_WAIT_BTN_A)/
 
-        ; Print 
+        ; Print
         VM_PUSH_CONST           0
         VM_PRINTER_DETECT       .ARG0, 10
-        VM_IF_CONST     .NE     .ARG0, 0, 104$, 0 
+        VM_IF_CONST     .NE     .ARG0, 0, 104$, 0
         VM_PRINT_OVERLAY        .ARG0, 0, 6
 104$:
         VM_POP                  1
@@ -133,19 +133,19 @@ _script_input_2::
 
 ;        VM_LOAD_PALETTE         0x01, ^/.PALETTE_COMMIT | .PALETTE_BKG/
 ;            .DMG_PAL    3,2,1,0
-        
+
         VM_PUSH_CONST           10
         VM_PUSH_CONST           ^/(.CAMERA_SHAKE_X | .CAMERA_SHAKE_Y)/
         VM_INVOKE               b_camera_shake_frames, _camera_shake_frames, 2, .ARG1
-        
+
 ;        VM_LOAD_PALETTE         0x01, ^/.PALETTE_COMMIT | .PALETTE_BKG/
 ;            .DMG_PAL    0,1,2,3
-        
+
 ; --- VM_SGB_TRANSFER example ------------------------
         VM_PUSH_CONST           0
-        VM_GET_INT8             .ARG0, __is_SGB 
-        VM_IF_CONST     .EQ     .ARG0, 0, 1$, 1 
-        VM_SGB_TRANSFER   
+        VM_GET_INT8             .ARG0, __is_SGB
+        VM_IF_CONST     .EQ     .ARG0, 0, 1$, 1
+        VM_SGB_TRANSFER
             .db ((0x04 << 3) | 1), 1, 0x07, ((0x01 << 4) | (0x02 << 2) | 0x03), 5,5, 10,10,  0, 0, 0, 0, 0, 0, 0, 0 ; ATTR_BLK packet
 1$:
 
@@ -153,11 +153,11 @@ _script_input_2::
         VM_RESERVE              6
         VM_RTC_START            .RTC_START
         VM_RTC_LATCH
-        VM_RTC_GET              .ARG3, .RTC_SECONDS 
-        VM_RTC_GET              .ARG2, .RTC_MINUTES 
-        VM_RTC_GET              .ARG1, .RTC_HOURS 
-        VM_RTC_GET              .ARG0, .RTC_DAYS 
-                
+        VM_RTC_GET              .ARG3, .RTC_SECONDS
+        VM_RTC_GET              .ARG2, .RTC_MINUTES
+        VM_RTC_GET              .ARG1, .RTC_HOURS
+        VM_RTC_GET              .ARG0, .RTC_DAYS
+
         VM_LOAD_TEXT            4
             .dw .ARG0, .ARG1, .ARG2, .ARG3
             .asciz "\002\001DAY:%D8 TIME: %D2:%D2:%D2"
@@ -165,7 +165,7 @@ _script_input_2::
         VM_OVERLAY_MOVE_TO      0, 9, .OVERLAY_TEXT_IN_SPEED
         VM_DISPLAY_TEXT
         VM_OVERLAY_SHOW         0, 9, .UI_COLOR_BLACK, 0
-        
+
 ; --- VM_OVERLAY_SET_SUBMAP_EX example ---------------
         VM_RPN
             .R_INT8     4               ; X coord
@@ -179,14 +179,14 @@ _script_input_2::
         VM_POP                  6       ; dispose parameters
 
         VM_OVERLAY_WAIT         .UI_MODAL, ^/(.UI_WAIT_WINDOW | .UI_WAIT_TEXT | .UI_WAIT_BTN_ANY)/
-        
+
 ; --- RPN Calculator example -------------------------
         VM_SET_CONST            .ARG5, 0
         VM_ACTOR_GET_POS        .ARG5
-        
+
         VM_SET_CONST            .ARG2, 1
         VM_ACTOR_GET_POS        .ARG2
-        
+
         VM_RPN
             ; chebyshev distance
             .R_REF      .ARG4
@@ -208,32 +208,32 @@ _script_input_2::
             .R_OPERATOR .SUB
             .R_OPERATOR .ABS
             .R_OPERATOR .ADD
-        
+
             .R_STOP
-        
+
         VM_LOAD_TEXT            6
             .dw .ARG6, .ARG5, .ARG3, .ARG2, .ARG1, .ARG0
             .asciz "\001\001\002\004@A\nBC\001\003\004\001\377\002\001x1=%d y1=%d\nx2=%d y2=%d\n\004\376\001Chebyshev:\002\002%d\n\002\001Manhattan:\002\002%d\n\002\001\007\002This\007\001 is \002\002BOLD\002\001\nOk!"
-        VM_OVERLAY_CLEAR        0, 0, 20, 9, .UI_COLOR_WHITE, .UI_DRAW_FRAME 
+        VM_OVERLAY_CLEAR        0, 0, 20, 9, .UI_COLOR_WHITE, .UI_DRAW_FRAME
         VM_DISPLAY_TEXT
         VM_OVERLAY_WAIT         .UI_MODAL, ^/(.UI_WAIT_WINDOW | .UI_WAIT_TEXT | .UI_WAIT_BTN_ANY)/
-        
+
         VM_LOAD_TEXT            0
             .asciz "\001\001\002\004@A\nBC\001\003\004\001\377\002\001Hi, it's KOT speaking!\nWhere's SLON?\006\377\003\022\005\001\001\002\004DE\nFG\003\006\005\001\003\002\001Sorry, i have no idea\nwhat're you mean!"
         VM_OVERLAY_MOVE_TO      0, 11, .OVERLAY_TEXT_OUT_SPEED
-        VM_OVERLAY_CLEAR        0, 0, 20, 7, .UI_COLOR_WHITE, .UI_DRAW_FRAME 
+        VM_OVERLAY_CLEAR        0, 0, 20, 7, .UI_COLOR_WHITE, .UI_DRAW_FRAME
         VM_DISPLAY_TEXT
         VM_OVERLAY_WAIT         .UI_MODAL, ^/(.UI_WAIT_WINDOW | .UI_WAIT_TEXT | .UI_WAIT_BTN_ANY)/
-        
-        VM_OVERLAY_CLEAR        0, 0, 20, 7, .UI_COLOR_WHITE, ^/(.UI_DRAW_FRAME | .UI_AUTO_SCROLL)/ 
+
+        VM_OVERLAY_CLEAR        0, 0, 20, 7, .UI_COLOR_WHITE, ^/(.UI_DRAW_FRAME | .UI_AUTO_SCROLL)/
         VM_LOAD_TEXT            0
             .asciz "\001\005Terminal mode\rLoading text...\rText loaded!\rLet's see how this text\rscrolls...\rOne more line...\rYes, it does!\rLet's type something\relse...\rI don't know what...\rHello, world!\rI think that is\r\002\002ENOUGH\002\001."
         VM_DISPLAY_TEXT
         VM_OVERLAY_WAIT         .UI_MODAL, ^/(.UI_WAIT_WINDOW | .UI_WAIT_TEXT | .UI_WAIT_BTN_ANY)/
-        
+
         VM_OVERLAY_MOVE_TO      0, 18, .OVERLAY_TEXT_OUT_SPEED
         VM_OVERLAY_WAIT         .UI_MODAL, ^/(.UI_WAIT_WINDOW)/
-        
+
         VM_POP                  ^/(6 + 2)/    ; 6 for local vars + 2 results of RPN calc
 
         ; Stop Script
