db10fd10a624e72d66b66d0321edd0f271fd7470
Chris Maltby <chris.maltby@gmail.com>
2024-01-08 21:11:02 +0000

    Updated develop example to include slopes

--- examples/develop/data/src/data/bg_platform_path.c
+++ examples/develop/data/src/data/bg_platform_path.c
@@ -3,7 +3,7 @@
 // Background: platform_path
 
 #include "gbs_types.h"
-#include "data/tileset_platform_path.h"
+#include "data/bg_platform_path_tileset.h"
 #include "data/bg_platform_path_tilemap.h"
 #include "data/bg_platform_path_tilemap_attr.h"
 
@@ -12,7 +12,7 @@ BANKREF(bg_platform_path)
 const struct background_t bg_platform_path = {
     .width = 161,
     .height = 18,
-    .tileset = TO_FAR_PTR_T(tileset_platform_path),
+    .tileset = TO_FAR_PTR_T(bg_platform_path_tileset),
     .cgb_tileset = { NULL, NULL },
     .tilemap = TO_FAR_PTR_T(bg_platform_path_tilemap),
     .cgb_tilemap_attr = TO_FAR_PTR_T(bg_platform_path_tilemap_attr)
