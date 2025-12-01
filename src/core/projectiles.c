04a05f785f058c1cf5373ebfb0048a0237f91fa4
Chris Maltby <chris.maltby@gmail.com>
2025-12-01 13:02:37 +0000

    projectiles_update now tests if collision_group is set to 'player' and only tests for collisions with player in that case

--- src/core/projectiles.c
+++ src/core/projectiles.c
@@ -65,7 +65,14 @@ void projectiles_update(void) NONBANKED {
         projectile->pos.y -= projectile->delta_pos.y;
 
         if (IS_FRAME_EVEN) {
-            actor_t *hit_actor = actor_overlapping_bb(&projectile->def.bounds, &projectile->pos, NULL);
+            actor_t *hit_actor = NULL;
+            if (projectile->def.collision_mask == COLLISION_GROUP_PLAYER) {
+                if  (bb_intersects(&projectile->def.bounds, &projectile->pos, &PLAYER.bounds, &PLAYER.pos)) {
+                    hit_actor = &PLAYER;
+                }
+            } else {
+                hit_actor = actor_overlapping_bb(&projectile->def.bounds, &projectile->pos, NULL);
+            }
             if (hit_actor && (hit_actor->collision_group & projectile->def.collision_mask)) {
                 // Hit! - Fire collision script here
                 if ((hit_actor->script.bank) && (hit_actor->hscript_hit & SCRIPT_TERMINATED)) {
